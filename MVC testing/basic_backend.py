26ed339758e9d96ba40c1f6e8bd819d6ff40a583
David-T10 <54846690+David-T10@users.noreply.github.com>
2019-09-22 15:09:24 +0100

    Add files via upload
    Completed dummy MVC implementation on food porducts

--- MVC testing/basic_backend.py
+++ MVC testing/basic_backend.py
@@ -1,4 +1,5 @@
 #MVC example inventory using C.R.U.D- Create Read Update Delete
+#basic_backend forms the basis of the MODEL in MVC (creating basic funtions required)
 import mvc_exceptions as mvc_exc
 
 items = list()
@@ -11,7 +12,7 @@ def create_item(name, price, quantity):
     else:
         items.append({'name': name, 'price': price, 'quantity': quantity})
 #mvc_exc checks if item has already been stored and if so passes the class ItemAlreadyStored
-def create_items(add_items):
+def create_items(app_items):
     global items
     items = app_items
     
@@ -29,4 +30,38 @@ def read_items():
 
 def update_item(name, price, quantity):
     global items
-    idxs_items = list(filter(lamba i_x: i_x[1]['name'] == name, enumerate(items)))
+    idxs_items = list(filter(lambda i_x: i_x[1]['name'] == name, enumerate(items)))
+    if idxs_items:
+        i, item_to_update = idxs_items[0][0], idxs_items[0][1]
+        items[i] = {'name': name, 'price': price, 'quantity': quantity}
+    else:
+        raise mvc_exc.ItemNotStored('Can not update "{}" because it is not stored'.format(name))
+def delete_item(name):
+    global items
+    idxs_items = list(filter(lambda i_x: i_x[1]['name'] == name, enumerate(items)))
+    if idxs_items:
+        i, item_to_delete = idxs_items[0][0], idxs_items[0][1]
+        del items[i]
+    else:
+        raise mvc_exc.ItemNotStored('Can not update "{}" because it is not stored'.format(name))
+
+'''def main():
+
+    my_items = [{'name': 'bread', 'price': 0.5, 'quantity': 20},
+                {'name': 'milk', 'price': 1.0, 'quantity': 10},
+                {'name': 'wine', 'price': 10.0, 'quantity': 5},]
+
+    create_items(my_items)
+    #create_item('bread', price=3.0, quantity=15)
+    #testing ItemAlreadyStored exception
+
+    #reading items in my_items
+    print('Stored Items are:')
+    print(read_items())
+
+    update_item('bread', 2.0, 10)
+    print(read_items())
+    
+
+if __name__=='__main__':
+    main()'''
